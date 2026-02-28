132e18934d2448f469b3a2d6b1ffee79bb442317
Alexander Demin <ademin@gmail.com>
2026-02-28 18:05:55 +0000

    Update SHA256 checksums for macOS paths

--- Formula/paths.rb
+++ Formula/paths.rb
@@ -7,10 +7,10 @@ class Paths < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/begoon/zig-path/releases/download/latest/paths-macos-arm64.tar.gz"
-      sha256 "9eaaca6bfe5abdc9dfd26a020f0482df8a3649b58279f2d4c6b18740bfa72ecc"
+      sha256 "aa34a7844533bde1b2210d7222c85c58bf422a6722b7f6429cf1902888d2618f"
     else
       url "https://github.com/begoon/zig-path/releases/download/latest/paths-macos-x86_64.tar.gz"
-      sha256 "c1f64892efe33781180438f3255558a3c29e322b5a8a9c17f1b7829cff1745e9"
+      sha256 "4c0cc24766dbbfebb07b6ea89c6ddfd1d76431c616d7e5a217d5f263e2673fe5"
     end
   end
 
